2a4bbbee37dcdd3257aad05923f81f3e824eb47c
Kabileshwarankabil <kabileshwaran1896@gmail.com>
2026-05-23 21:42:07 +0530

    numpy

--- numpy/.venv/Scripts/activate.bat
+++ numpy/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\AcademyHub\Self Learning\machine-learning\numpy\.venv
+set VIRTUAL_ENV=D:\AcademyHub\Self Learning\machine-learning\NumPy\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
